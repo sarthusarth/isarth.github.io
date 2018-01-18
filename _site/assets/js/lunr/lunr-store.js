9a44727b760b159e4981e0105163382d960a8024
isarth <isarth@me.com>
2018-01-19 02:21:54 +0530

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"gdfgddgfgd","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/hello/",
+        "teaser":null}]
