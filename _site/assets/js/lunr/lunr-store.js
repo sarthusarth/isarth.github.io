dda11952dc04cbb874248e271042d00e539669cd
isarth <isarth@me.com>
2018-01-21 02:22:00 +0530

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Welcome to Jekyll!",
-        "excerpt":"This is my first post","categories": ["jekyll","update"],
+        "excerpt":"This is my first post","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/hello/",
+        "url": "http://localhost:4000/hello/",
         "teaser":"http://localhost:4000/assets/images/earth.jpg"}]
