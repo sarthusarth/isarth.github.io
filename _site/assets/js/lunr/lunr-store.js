f734bfb5a97285ac29f6b59c2e5fc62ba4136a5a
isarth <isarth@me.com>
2018-08-23 02:46:02 +0530

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"This is my first post","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/hello/",
-        "teaser":"http://localhost:4000/assets/images/earth.jpg"},{
         "title": "My Macbook Setup",
         "excerpt":"Must intall apps on macbook","categories": [],
         "tags": [],
         "url": "http://localhost:4000/macsetup/",
-        "teaser":"http://localhost:4000/assets/images/mac2.jpg"}]
+        "teaser":"http://localhost:4000/assets/images/mac2.jpg"},{
+        "title": "Extractive Text Summary using TextRank ",
+        "excerpt":"This is my first post","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/hello/",
+        "teaser":"http://localhost:4000/assets/images/earth.jpg"}]
