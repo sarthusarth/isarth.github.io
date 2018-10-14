e6375047eacc8872a8a378f31c2f49048ef64c3a
isarth <isarth@me.com>
2018-10-15 03:03:58 +0530

    pagerank

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
         "title": "Extractive Text Summary using TextRank ",
-        "excerpt":"This is my first post","categories": [],
+        "excerpt":"Upcoming Blog","categories": [],
         "tags": [],
         "url": "http://localhost:4000/hello/",
         "teaser":null},{
         "title": "PageRank",
-        "excerpt":"In simple terms Page Rank is an intutive way of ranking web pages. Which formed the basis for Google’s web indexing algorithm. Belive it or not Page Rank is very intutive algorithm. Which I will be explaing in this post. The challenging part about Page Rank is the scaling. But...","categories": [],
+        "excerpt":"In simple terms, PageRank is an intuitive way of ranking web pages. Which formed the basis for Google’s web indexing algorithm during its early phase. Believe it or not Page Rank is a very intuitive algorithm, which is why I wrote this post (it is that easy!). The only challenging...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/pagerank/",
         "teaser":"http://localhost:4000/assets/images/google-logo.png"}]
