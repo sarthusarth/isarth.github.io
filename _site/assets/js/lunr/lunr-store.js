65a95016c9df3911ba47694c1f2562acae1ac47d
isarth <isarth@me.com>
2018-10-16 00:11:57 +0530

    final

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,6 @@
 var store = [{
-        "title": "Extractive Text Summary using TextRank ",
-        "excerpt":"Still in progress","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/hello/",
-        "teaser":null},{
         "title": "PageRank",
-        "excerpt":"In simple terms, PageRank is an intuitive way of ranking web pages, which formed the basis for google’s web indexing algorithm during its early phase. Believe it or not Page Rank is a very intuitive algorithm. In this article, you’ll learn about the ituition behind page rank and implementing page...","categories": [],
+        "excerpt":"In simple terms, PageRank is an intuitive way of ranking web pages, which formed the basis for google’s web indexing algorithm during its early phase. In this article, you’ll learn about the ituition behind page rank and implementing page rank in python. The article is divided into following sections: Basic...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/pagerank/",
-        "teaser":"http://localhost:4000/assets/images/google-logo.png"}]
+        "teaser":null}]
