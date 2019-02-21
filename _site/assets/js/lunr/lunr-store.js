030d85fcdfa1c3c94a5dca3b513548d4be50cd89
isarth <isarth@me.com>
2019-02-22 00:52:50 +0530

    new

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "PageRank",
+        "title": "Understanding PageRank",
         "excerpt":"In simple terms, PageRank is an intuitive way of ranking web pages, which formed the basis for Google’s web indexing algorithm during its early phase. In this article, you’ll learn about the intuition behind page rank and implementing page rank in python. The article is divided into the following sections:...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/pagerank/",
